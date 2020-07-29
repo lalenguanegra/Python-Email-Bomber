eafc4df4a09dc7a49ed1efd0b04dc75e4b56831b
Hunt The Alien <36826771+lalenguanegra@users.noreply.github.com>
2020-07-29 16:03:10 -0700

    Create cock.li.py

---
+++ cock.li.py
@@ -0,0 +1,41 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+import time
+
+def test():
+    with open('1.txt', 'r') as f:
+       for line in f:
+         temp = line.rstrip('\n')
+         print(temp)
+         time.sleep(5)
+         fromaddr = "MY_EMAIL@cock.li"
+         toaddr = "TARGETS_EMAIL"
+
+         msg = MIMEMultipart()
+
+         msg['From'] = fromaddr
+         msg['To'] = toaddr
+         msg['Subject'] = "haha"
+		
+         body = temp
+
+
+
+         msg.attach(MIMEText(body, 'plain'))
+
+
+         server = smtplib.SMTP('mail.cock.li', 587)
+         server.starttls()
+         server.login(fromaddr, "MY_PASSWORD")
+         text = msg.as_string()
+         server.sendmail(fromaddr, toaddr, text)
+         server.quit()
+         
+
+numberEmails = 5
+
+for _ in range(numberEmails):
+    test()
